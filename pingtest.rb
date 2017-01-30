40217a55f005f07f74e06e6e4678e39d4733a7fa
Colton <colton.glasgow@outlook.com>
2017-01-30 12:57:32 -0800

    Update pingtest.rb

--- pingtest.rb
+++ pingtest.rb
@@ -5,10 +5,10 @@ puts "\e[H\e[2J"
 		sleep(0.5)
 end
 
-system('ping localhost -c 3 > /etc/chef/staticiplog.txt')
-system('ping google.com -c 3 > /etc/chef/staticipgooglog.txt')
+system('ping localhost -c 3 > /tmp/staticiplog.txt')
+system('ping google.com -c 3 > /tmp/staticipgooglog.txt')
 
-File.open '/etc/chef/staticipgooglog.txt' do |file|
+File.open '/tmp/staticipgooglog.txt' do |file|
 	if file.find { |line| line =~ /3 received/ }
 	puts "\e[H\e[2J"
 	puts 'Successfuly reached google.com.'
@@ -17,15 +17,17 @@ File.open '/etc/chef/staticipgooglog.txt' do |file|
 	end
 	end
 	
-File.open '/etc/chef/staticiplog.txt' do |file|
+File.open '/tmp/staticiplog.txt' do |file|
 	if file.find { |line| line =~ /3 received/ }
 	puts 'Successfuly reached localhost.' 
 	else 
 	puts "\e[H\e[2J"
 	puts 'Cannot ping localhost. Press "r" to retry'
+		
+		
 	while ans = gets.chomp
 	case ans
-	#r
+	#retrying
 	when 'r'
 	puts "\e[H\e[2J"
 		#retrying
@@ -34,10 +36,10 @@ File.open '/etc/chef/staticiplog.txt' do |file|
 		$stdout.flush
 		sleep(0.5)
 		end
-		system("ruby /etc/chef/staticip.rb")
+		system("ruby ~/chef-repo/dl-elk/files/default/staticip.rb")
 
-system('rm /etc/chef/staticiplog.txt')
-system('rm /etc/chef/staticipgooglog.txt')
+system('rm /tmp/staticiplog.txt')
+system('rm /tmp/staticipgooglog.txt')
 end
 end
 end
