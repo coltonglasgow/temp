262871d13fb3582f3e42d1b4a48d05b422e26125
Colton <colton.glasgow@outlook.com>
2017-01-31 16:28:03 -0800

    Update boxconfig.rb

--- boxconfig.rb
+++ boxconfig.rb
@@ -1,15 +1,15 @@
 #DNS
- system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "DNS1=.*" >> /etc/dl/dl-elk/files/default/')
- system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "DNS2=.*" >> /etc/dl/dl-elk/files/default/')
+ system('cat ~/chef-repo/dl-elk/files/default/ipinfo.txt | grep "DNS1=.*" >> ~/chef-repo/dl-elk/files/default/resolv.conf')
+ system('cat ~/chef-repo/dl-elk/files/default/ipinfo.txt | grep "DNS2=.*" >> ~/chef-repo/dl-elk/files/default/resolv.conf')
        system('mv ~/chef-repo/dl-elk/files/default/resolv.conf /etc/resolv.conf')
 
 #default gateway
- system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "DEFAULT=.*" >> /etc/dl/dl-elk/files/default/network')
+ system('cat ~/chef-repo/dl-elk/files/default/ipinfo.txt | grep "DEFAULT=.*" >> ~/chef-repo/dl-elk/files/default/network')
        system('mv ~/chef-repo/dl-elk/files/default/network /etc/sysconfig/network')
 
 #STATIC IP
- system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "IPADDR=.*" >> /etc/dl/dl-elk/files/default/ifcfg-enp0s3')
- system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "NETMASK=.*" >> /etc/dl/dl-elk/files/default/ifcfg-enp0s3')
+ system('cat ~/chef-repo/dl-elk/files/default/ipinfo.txt | grep "IPADDR=.*" >> ~/chef-repo/dl-elk/files/default/ifcfg-enp0s3')
+ system('cat ~/chef-repo/dl-elk/files/default/ipinfo.txt | grep "NETMASK=.*" >> ~/chef-repo/dl-elk/files/default/ifcfg-enp0s3')
        system('mv ~/chef-repo/dl-elk/files/default/ifcfg-enp0s3 /etc/sysconfig/network-scripts/ifcfg-enp0s3')
 
 #user and root pw
