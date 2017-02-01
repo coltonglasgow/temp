af1050d76e8acba66b9dd72c0ea5bc840f4a641e
Colton <colton.glasgow@outlook.com>
2017-01-31 16:10:57 -0800

    Update boxconfig.rb

--- boxconfig.rb
+++ boxconfig.rb
@@ -1,17 +1,20 @@
 #DNS
  system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "DNS1=.*" >> /etc/dl/dl-elk/files/default/')
  system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "DNS2=.*" >> /etc/dl/dl-elk/files/default/')
+       system('mv ~/chef-repo/dl-elk/files/default/resolv.conf /etc/resolv.conf')
 
 #default gateway
- system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "DEFAULT=.*" >> /etc/dl/dl-elk/files/default/resolv.conf')
+ system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "DEFAULT=.*" >> /etc/dl/dl-elk/files/default/network')
+       system('mv ~/chef-repo/dl-elk/files/default/network /etc/sysconfig/network')
 
 #STATIC IP
  system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "IPADDR=.*" >> /etc/dl/dl-elk/files/default/ifcfg-enp0s3')
  system('cat /etc/dl/dl-elk/files/default/ipinfo.txt | grep "NETMASK=.*" >> /etc/dl/dl-elk/files/default/ifcfg-enp0s3')
+       system('mv ~/chef-repo/dl-elk/files/default/ifcfg-enp0s3 /etc/sysconfig/network-scripts/ifcfg-enp0s3')
 
 #user and root pw
- system('sudo echo "linuxpassword" | passwd --stdin user')
- system('sudo echo "linuxpassword" | passwd --stdin root')
+ system('sudo echo "linuxpassword1" | passwd --stdin user')
+ system('sudo echo "linuxpassword2" | passwd --stdin root')
 
 #kibana user and pw
  system('htpasswd -b -c /etc/nginx/htpasswd.users username password')
